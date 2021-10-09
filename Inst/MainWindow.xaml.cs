03c80413d786763fa117e6ee10d666146b870dd9
1kamma <saretbenny@gmail.com>
2021-10-09 23:07:19 +0300

    main window modifications

--- Inst/MainWindow.xaml.cs
+++ Inst/MainWindow.xaml.cs
@@ -1,19 +1,19 @@
 using Installizer;
 using System.Windows;
-namespace Inst
-{
+namespace Inst {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public string? TaskName;
+        public string? TaskPath;
+        public string? FilePath;
+        public string? ArugmentLists;
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e) {
             Tasker tasker = new Tasker("ab", "cd");
             int[] i = new int[] { 1, 2 };
             tasker.TaskActionsDefine(@"C:\Program Files\PowerShell\7\pwsh.exe", null, null);
@@ -24,14 +24,37 @@ namespace Inst
             System.Console.WriteLine("now");
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
 
         }
 
-        private void SvgCanvas_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
-        {
+        private void SvgCanvas_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e) {
 
+        }
+
+        private void TaskName_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+            TaskName = e.OriginalSource.ToString();
+        }
+
+        private void TaskPath_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+            TaskPath = e.OriginalSource.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) {
+            using (System.Windows.Forms.OpenFileDialog fileDialog = new()) {
+                fileDialog.Filter = "exe files (*.exe)|*.exe";
+                fileDialog.Title = "Start a file";
+                fileDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+                if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                    FilePath = fileDialog.FileName;
+                }
+            }
+            Pathus.Text = FilePath;
+
+        }
+
+        private void Argusy_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+            ArugmentLists = e.OriginalSource.ToString();
         }
     }
 }
